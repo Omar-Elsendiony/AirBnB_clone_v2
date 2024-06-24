9db154bb853f64994a34ea99880d900373ae6d65
Omar Elsendiony <omar.sendiony@gmail.com>
2024-06-24 09:54:45 +0300

    small error

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ import os
 from models.engine.file_storage import FileStorage
 
 storage_t = os.getenv('HBNB_TYPE_STORAGE')
-print(storage_t)
+# print(storage_t)
 if (os.getenv('HBNB_TYPE_STORAGE') == 'db'):
     from models.engine.db_storage import DBStorage
     storage_t = "db"
